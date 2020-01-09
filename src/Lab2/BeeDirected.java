a4d4185938639a7057406b067228d6792db3d9c9
Mueller <muellerbs@ad.msoe.edu>
2020-01-09 15:37:04 -0600

    refined descriptions of the different types of bees

--- src/Lab2/BeeDirected.java
+++ src/Lab2/BeeDirected.java
@@ -11,6 +11,10 @@ package Lab2;
  * CS1021-081 Winter 2018-2019
  * Class purpose: directed bee
  *
+ * This bee moves through the garden in a set, directed fashion.
+ * This is opposed to the random bee that chooses a flower and moves in
+ * a straight line towards it.
+ *
  * @author drazek-loosierc
  * @version created on 12/17/2019 at 7:27 PM
  */
--- src/Lab2/BeeRandom.java
+++ src/Lab2/BeeRandom.java
@@ -11,6 +11,10 @@ package Lab2;
  * CS1021-081 Winter 2018-2019
  * Class purpose: random bee
  *
+ *
+ * This bee chooses a flower at random, and moves straight towards it.
+ * This is opposed to the other bee that moves in a set, directed fashion
+ * through the garden.
  * @author drazek-loosierc
  * @version created on 12/17/2019 at 7:28 PM
  */
