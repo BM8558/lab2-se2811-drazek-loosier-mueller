2b5f64f64da09bca1ae0e571320385e5683998cf
ChrisDrazek <drazek-loosierc@msoe.edu>
2020-01-16 13:39:02 -0600

    initial controller implementation

--- src/Lab2/GardenController.java
+++ src/Lab2/GardenController.java
@@ -10,8 +10,11 @@ package Lab2;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 /**
@@ -27,9 +30,57 @@ public class GardenController {
     @FXML
     private Pane gardenPane;
 
+    private ArrayList<Bee> beeList = new ArrayList<>();
+    private ArrayList<Flower> flowerList = new ArrayList<>();
+    private ArrayList<Entity> entityList = new ArrayList<>();
+
     public void initialize() {
         gardenPane.setStyle("-fx-background-color: linear-gradient(to bottom right," +
                 " derive(forestgreen, 20%), derive(forestgreen, -40%));");
 
+        beeList.add(new BeeDirected(100, 100));
+        beeList.add(new BeeRandom(600, 800));
+        entityList.addAll(beeList);
+        flowerList.add(new GoodFlower(450, 100));
+        flowerList.add(new GoodFlower(450, 600));
+        flowerList.add(new BadFlower(100, 350));
+        flowerList.add(new BadFlower(800, 350));
+        entityList.addAll(flowerList);
+        for (Entity obj: entityList) {
+            gardenPane.getChildren().add(obj.getImageView());
+        }
+        tick();
+    }
+
+    @FXML
+    public void onKeyPressed(KeyEvent keyEvent) {
+        tick();
+    }
+
+    private void tick() {
+        moveAll();
+        checkCollision();
+        updateDisplay();
+    }
+
+    private void moveAll() {
+        for (Bee bee: beeList) {
+            bee.move(flowerList);
+        }
+    }
+
+    private void checkCollision() {
+        for (int i = 0; i < entityList.size(); i++) {
+            for (int j = i + 1; j < entityList.size(); j++) {
+                entityList.get(i).collide(entityList.get(j));
+            }
+        }
+    }
+
+    private void updateDisplay() {
+        for (Entity obj: entityList) {
+            obj.getImageView().setX(obj.getXPos());
+            obj.getImageView().setY(obj.getYPos());
+        }
     }
 }
