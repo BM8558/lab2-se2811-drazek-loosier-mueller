6fdd06a80221346ea0b2c98a9af996fded720795
Mueller <muellerbs@ad.msoe.edu>
2020-01-21 23:04:29 -0600

    most of movement method working

--- src/Lab2/BeeRandom.java
+++ src/Lab2/BeeRandom.java
@@ -19,7 +19,7 @@ import java.util.List;
  * This bee chooses a flower at random, and moves straight towards it.
  * This is opposed to the other bee that moves in a set, directed fashion
  * through the garden.
- * @author drazek-loosierc
+ * @author muellerbs
  * @version created on 12/17/2019 at 7:28 PM
  */
 public class BeeRandom extends Bee {
@@ -31,6 +31,7 @@ public class BeeRandom extends Bee {
     int xPos, yPos, energy;
     static final double MOVE_DISTANCE = 10.0;
     ImageView imageView;
+    Flower target;
 
     /**
      * constructor for the random selecting bee
@@ -54,13 +55,52 @@ public class BeeRandom extends Bee {
      * @param flowerList the list of flowers
      */
     public void chooseTarget(List<Flower> flowerList ){
-        //chooses a target from the list of flowers
+        int random = (int)(Math.random() * ((flowerList.size())));
+        target = flowerList.get(random);
     }
 
     /**
      * Moves the bee a fixed distance towards the target flower.
      */
-    public void move(){
+    public void move(List<Flower> flowerList){
+        if(target == null){
+            chooseTarget(flowerList); //sets a new target if the old target was reached
+        }
+        double targetX = target.getImageView().getX();
+        double targetY = target.getImageView().getY();
+        double currentX = imageView.getX();
+        double currentY = imageView.getY();
+        double newX, newY;
 
+
+        /*
+        gets the new x value for the bee based on the target
+         */
+        if(Math.abs(targetX - currentX) > 10.0){
+            if(targetX > currentX){
+                newX = currentX + 10.0;
+            } else {
+                newX = currentX - 10.0;
+            }
+        } else{
+            newX = targetX;
+        }
+
+        /*
+        gets the new y value for the bee based on the target
+         */
+        if(Math.abs(targetY - currentY) > 10.0){
+            if(targetY > currentY){
+                newY = currentY + 10.0;
+            } else {
+                newY = currentY - 10.0;
+            }
+        } else{
+            newY = targetY;
+        }
+
+        imageView.setX(newX);
+        imageView.setY(newY);
     }
+    
 }
